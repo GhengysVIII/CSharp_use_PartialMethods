b1c0f0be9f8817d875f35a62faa491ae70f72935
GhengysVIII <34130506+GhengysVIII@users.noreply.github.com>
2020-02-20 15:16:46 +0100

    Add files via upload

---
+++ use_PartialMethods/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace use_PartialMethods {
+    class Program {
+        static void Main(string[] args) {
+            Voiture v = new Voiture();
+            v.Demarrer(); // Appel de la methode publique
+        }
+    }     
+}
---
+++ use_PartialMethods/Voiture.cs
@@ -0,0 +1,22 @@
+﻿namespace use_PartialMethods {
+
+    // Partie 1
+    partial class Voiture { // Dans une classe partielle obligé !
+        
+        partial void AuDemarrage(); // Déclaration de la signature de la methode partielle
+                                    // Ne peut pas avoir de modificateur d'accès !
+                                    // Doit avoir un type de retour void !
+                                    // Peut être une methode statique
+                                    
+
+        public void Demarrer() => AuDemarrage(); // Methode d'accès à la methode partielle
+                                                 // (On ne peut pas avoir accès à la methode partielle directement)
+    }
+
+    partial class Voiture { // Classe partielle bis
+
+        partial void AuDemarrage() =>                // Corps de la methode partiel 
+            System.Console.WriteLine("VROUUUUUM");
+       
+    }
+}
